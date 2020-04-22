6eebcb608a45ee13b8e7d434061b66a9d8fbe3f2
Tushar Kumar <tushar.ku@HCL.COM>
2020-04-22 14:18:14 +0530

    windows utility

---
+++ delete d1.cmd
@@ -0,0 +1,10 @@
+del /f /s /q "d1" > NUL
+rmdir /s /q "d1"
+del /f /s /q "d2" > NUL
+rmdir /s /q "d2"
+del /f /s /q "d3" > NUL
+rmdir /s /q "d3"
+del /f /s /q "d4" > NUL
+rmdir /s /q "d4"
+del /f /s /q "d5" > NUL
+rmdir /s /q "d5"
---
+++ kill.bat
@@ -0,0 +1,4 @@
+cmd "/C TASKKILL /IM node.exe /F"
+cmd "/C TASKKILL /IM electron.exe /F"
+cmd "/C TASKKILL /IM cmd.exe /F"
+cmd "/C TASKKILL /IM conhost.exe /F"
